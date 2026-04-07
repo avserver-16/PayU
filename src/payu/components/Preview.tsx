38fcf5805b0bb0b1715a7d575c3f4365d3bf8c00
avserver-16 <avish.vijay2021@gmail.com>
2026-04-08 01:53:14 +0530

    feat; replicated 1:1 ui with figma for profile tab completely

---
+++ src/payu/components/Preview.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Preview: React.FC = () => {
+  const data = [
+    ['Total Spendings:', '$2000'],
+    ['Email:', 'alex@gmail.com'],
+    ['Balance:', '$20000'],
+  ];
+
+  return (
+    <View style={styles.container}>
+      {data.map((row, rowIndex) => (
+        <View key={rowIndex} style={styles.row}>
+          {row.map((item, colIndex) => {
+            const isValue = colIndex === 1; 
+
+            return (
+              <View
+                key={colIndex}
+                style={[
+                  styles.box,
+                  isValue ? styles.valueBox : styles.labelBox,
+                ]}
+              >
+                <Text
+                  style={[
+                    
+                    isValue ? styles.valueText : styles.labelText,
+                  ]}
+                >
+                  {item}
+                </Text>
+              </View>
+            );
+          })}
+        </View>
+      ))}
+    </View>
+  );
+};
+
+export default Preview;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingVertical:24
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 12,
+  },
+  box: {
+    width: '48%',
+  },
+
+  // LEFT → Labels
+  labelBox: {
+    alignItems: 'flex-start',
+  },
+  labelText: {
+    color: '#A1A1A1',
+    fontSize: 16,
+  },
+
+  // RIGHT → Values
+  valueBox: {
+    alignItems: 'flex-end',
+  },
+  valueText: {
+    color: '#FAFAFA',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+});
--- src/payu/profile.tsx
+++ src/payu/profile.tsx
@@ -1,13 +1,46 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { User } from 'lucide-react-native';
 import GradientBackground from '../../styles/Background';
 import Header from './components/Header';
+import Logo from '../../styles/Logo';
+import Preview from './components/Preview';
+import Signup from '../auth/Signup';
 
 const ProfilePage = () => {
+  const [period, setPeriod] = useState<'preview' | 'edit'>('preview');
+
   return (
     <GradientBackground>
       <Header title="PayU" />
+      <View style={{ flex: 1, paddingVertical: 24 }}>
+        <View style={{ flexDirection: 'row', gap: 10, alignItems: 'center', marginBottom: 24 }}>
+          <Logo width={32} height={32} borderRadius={10} />
+          <Text style={{ fontSize: 16, fontWeight: 'bold', color: '#FAFAFA', top: -6 }}>Alex Lyu</Text>
+        </View>
+        <View style={styles.toggleContainer}>
+          <TouchableOpacity
+            style={[styles.toggleButton, period === 'preview' && styles.activeToggle]}
+            onPress={() => setPeriod('preview')}
+          >
+            <Text style={[styles.toggleText, period === 'preview' && styles.activeToggleText]}>
+              Preview
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={[styles.toggleButton, period === 'edit' && styles.activeToggle]}
+            onPress={() => setPeriod('edit')}
+          >
+            <Text style={[styles.toggleText, period === 'edit' && styles.activeToggleText]}>
+              Edit
+            </Text>
+          </TouchableOpacity>
+        </View>
+        {
+          period === 'preview' ? <Preview /> : <View style={{ flex: 1 ,top:-24}}><Signup buttonText="Update Profile" /></View>
+        }
+      </View>
     </GradientBackground>
   );
 };
@@ -31,6 +64,30 @@ const styles = StyleSheet.create({
   subtitle: {
     fontSize: 16,
     color: '#666',
+  },
+  toggleContainer: {
+    flexDirection: 'row',
+    backgroundColor: '#262626',
+    borderRadius: 100,
+    padding: 8,
+    justifyContent: 'space-between',
+    // marginBottom: 24,
+  },
+  toggleButton: {
+    flex: 1,
+    paddingVertical: 4,
+    alignItems: 'center',
+    borderRadius: 100,
+  },
+  activeToggle: {
+    backgroundColor: '#FAFAFA',
+  },
+  toggleText: {
+    color: '#A1A1A1',
+    fontSize: 14,
+  },
+  activeToggleText: {
+    color: '#000',
   },
 });
 
