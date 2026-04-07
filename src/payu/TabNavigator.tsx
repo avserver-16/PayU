ddae23cad3bb41b40b46901b89826839e41d7e66
avserver-16 <avish.vijay2021@gmail.com>
2026-04-07 18:38:34 +0530

    fix: ui replicaion for tab bar

--- src/payu/TabNavigator.tsx
+++ src/payu/TabNavigator.tsx
@@ -10,21 +10,34 @@ const Tab = createBottomTabNavigator();
 const TabNavigator = () => {
   return (
     <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          if (route.name === 'Home') {
-            return <Home size={size} color={color} />;
-          } else if (route.name === 'Balance') {
-            return <DollarSign size={size} color={color} />;
-          } else if (route.name === 'Profile') {
-            return <User size={size} color={color} />;
-          }
-        },
-        tabBarActiveTintColor: '#007AFF',
-        tabBarInactiveTintColor: 'gray',
-        headerShown: false,
-      })}
-    >
+  screenOptions={({ route }) => ({
+    tabBarIcon: ({ color, size }) => {
+      if (route.name === 'Home') {
+        return <Home size={size} color={color} />;
+      } else if (route.name === 'Balance') {
+        return <DollarSign size={size} color={color} />;
+      } else if (route.name === 'Profile') {
+        return <User size={size} color={color} />;
+      }
+    },
+
+   
+    tabBarActiveTintColor: '#FAFAFA',
+    tabBarInactiveTintColor: 'gray',
+
+  
+    tabBarStyle: {
+      backgroundColor: '#0A0A0A',
+      borderTopWidth: 0.3,
+      borderColor: '#000000',
+      height: 70,
+      paddingBottom: 10,
+      paddingTop: 6,
+    },
+
+    headerShown: false,
+  })}
+>
       <Tab.Screen 
         name="Home" 
         component={HomePage}
