a9c2b395f670a0b75605096c8d466de971fffd56
avserver-16 <avish.vijay2021@gmail.com>
2026-04-09 01:13:23 +0530

    feat: added tracker for credit score

--- src/payu/Balance.tsx
+++ src/payu/Balance.tsx
@@ -6,6 +6,7 @@ import Header from './components/Header';
 import SemiCircle from './components/Semicircle';
 import ExpenseCard from './components/ExpenseCard';
 import TimelineBarChart from './components/GradientBarGraph';
+import SemiCirclePieChart from './components/Semicircle';
 
 const BalancePage = () => {
   return (
@@ -20,15 +21,28 @@ const BalancePage = () => {
           <Text style={{ color: '#A1A1A1', fontSize: 16, fontWeight: 'regular' }}>Manage your multi-currency accounts</Text>
         </View>
         <View style={{ marginTop: 24 }}>
-          <SemiCircle value="660" title="Your Credit Score is average" subtitle="Last Check on 21 Apr" />
+          <SemiCirclePieChart
+            title="Your Credit score is average"
+            subtitle="Last check on 21 April"
+            size={300}
+            strokeWidth={24}
+            gapDeg={5}
+            valueText={660}
+            data={[
+              { label: "Good", value: 600, color: "#3FB9A2"},
+              { label: "Better", value: 200, color: "#EE89DF", colorEnd: "#EE89DF" },
+              { label: "Best", value: 150, color: "#74B8EF" },
+              { label: "Marvelous", value: 50, color: "#F6D2B3" },
+            ]}
+          />
         </View>
-        <View style={{ marginTop: 48, gap: 18 }}>
+        <View style={{ marginTop: 24, gap: 18 }}>
           <Text style={{ color: '#FAFAFA', fontSize: 16, fontWeight: 'semibold', marginBottom: 12 }}>Available Currencies</Text>
           <ExpenseCard title="USD" description="United States Dollar" price="+ Enable" colors={["#262626", "#0A0A0A"]} />
-          <ExpenseCard title="CAD" description="Canadian Dollar" price="+ Enable" colors={["#262626", "#0A0A0A"]} />
-          <ExpenseCard title="EUR" description="Euro" price="+ Enable" colors={["#262626", "#0A0A0A"]} />
+          {/* <ExpenseCard title="CAD" description="Canadian Dollar" price="+ Enable" colors={["#262626", "#0A0A0A"]} /> */}
+          {/* <ExpenseCard title="EUR" description="Euro" price="+ Enable" colors={["#262626", "#0A0A0A"]} /> */}
         </View>
-        <View style={{ marginTop: 48 }}>
+        <View style={{ marginTop: 24 }}>
           <TimelineBarChart />
           <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginTop: 48 }}>
             <Text style={{ color: '#7A7A7A', fontSize: 12, fontWeight: 'semibold' }}>Current Margin: April Spendings</Text>
